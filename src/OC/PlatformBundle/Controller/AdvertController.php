b26e4a8e9ab2af38f74934c0c5947b3f14bb217a
Quentin84 <quentinst@gmail.com>
2016-03-20 18:48:35 +0100

    Debut chapitre controller
    Redirections, utilisation templates, etc

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,6 +6,8 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
@@ -15,14 +17,20 @@ class AdvertController extends Controller
                 'oc_platform_view', // 1 Nom de la route
                 array('id' => 5) // 2 tableau avec les parametres
                 );
-        return new Response('<br>Et l\'url est :'.$url);
+        return new RedirectResponse($url);
     }
-    public function viewAction($id){
-        
-        return new Response('truc avec l\'id'.$id);
+    public function viewAction($id, request $request){
+        $tag= $request->query->get('tag');
+        return $this->render(
+                'OCPlatformBundle:Advert:view.html.twig',
+                array('id'  => $id)
+                );
     }
     public function viewSlugAction($slug, $year, $_format){
         return new Response(
             "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format.".");
+    }
+    public function redirectAction(){
+        return $this->redirectToRoute('oc_platform_home');
     }
 }
