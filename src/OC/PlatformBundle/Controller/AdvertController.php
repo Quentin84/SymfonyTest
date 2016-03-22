cb1d31a66d1edf70c27e92c37729f969e77b23dc
Quentin84 <quentinst@gmail.com>
2016-03-22 16:43:48 +0100

    controllers de test

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+//Json
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdvertController extends Controller
 {
@@ -21,6 +23,9 @@ class AdvertController extends Controller
     }
     public function viewAction($id, request $request){
         $tag= $request->query->get('tag');
+        $session = $request->getSession();
+        $userID = $session->get('user_id');
+        $session->set('user_id', 91);
         return $this->render(
                 'OCPlatformBundle:Advert:view.html.twig',
                 array('id'  => $id)
@@ -33,4 +38,23 @@ class AdvertController extends Controller
     public function redirectAction(){
         return $this->redirectToRoute('oc_platform_home');
     }
+    public function getJsonAction(){
+        return new JsonResponse(array('banane', 'framboise', 'chaussette'));
+    }
+    public function addAction(Request $request){
+        $session = $request->getSession();
+    
+        // Bien sûr, cette méthode devra réellement ajouter l'annonce
+    
+        // Mais faisons comme si c'était le cas
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+        // Le « flashBag » est ce qui contient les messages flash dans la session
+        // Il peut bien sûr contenir plusieurs messages :
+        $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+
+        // Puis on redirige vers la page de visualisation de cette annonce
+        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+
 }
